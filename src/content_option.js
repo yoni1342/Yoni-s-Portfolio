59ac9dc51d0baa7649586a7cf7c929d700a02b86
Yonatan Tesfaye <yonatantesfaye@pop-os.localdomain>
2023-03-20 15:56:28 +0300

    Final

---
+++ src/content_option.js
@@ -0,0 +1,162 @@
+const logotext = "YONI";
+const meta = {
+    title: "Yonatan Tesfaye",
+    description: "I’m Yonatan Tesfate _ Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Yonatan Tesfaye",
+    animated: {
+        first: "I'm a man of solution",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Welcome to my portfolio! I am a passionate and skilled web developer who specializes in creating beautiful, user-friendly, and high-performing websites and applications.",
+    your_img_url: "https://ik.imagekit.io/s3tynwsqe/photo_2023-03-20_11-11-18_1S_F3vsRT.jpg?updatedAt=1679299937343",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "As a full-stack developer and problem solver, I bring a diverse set of talents and skills to the table. With experience in both front-end and back-end development, I have a well-rounded understanding of building mobile and web applications. My ability to approach challenges with creativity and perseverance, finding innovative solutions that meet the needs of my clients, makes me a valuable asset in any project. Additionally, I am passionate about entrepreneurship, and I am always eager to take on new challenges and pursue opportunities that align with my interests and goals. Overall, I am a skilled, motivated, and passionate individual who is dedicated to delivering top-notch results.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "Figma",
+        value: 90,
+    },
+    {
+        name: "Node.Js",
+        value: 85,
+    },
+    {
+        name: "Python",
+        value: 85,
+    },
+    {
+        name: "React.Js",
+        value: 80,
+    },
+    {
+        name: "React Native",
+        value: 80,
+    },
+    {
+        name: "MongoDB",
+        value: 75,
+    },
+    {
+        name: "Postgresql",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "As a UI/UX designer, I am passionate about creating digital experiences that are both beautiful and user-friendly. I believe that design should not only be visually appealing but also functional, intuitive, and accessible to everyone. I have experience designing websites, mobile apps, and software interfaces across various industries, including healthcare, education, e-commerce, and more. I am proficient in design tools such as Sketch, Figma, Adobe Creative Suite, and InVision.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "As a mobile app developer, I am passionate about building high-quality and performant apps that meet the needs of their users. I have experience developing mobile apps for both iOS and Android platforms, using a variety of programming languages and frameworks.",
+    },
+    {
+        title: "Web Apps",
+        description: "As a web app developer, I am passionate about building high-quality, responsive, and scalable web apps that meet the needs of their users. I have experience developing web apps using a variety of programming languages and frameworks. I have experience developing web apps for various industries, including healthcare, finance, education, and more. I am proficient in programming languages such as JavaScript, TypeScript, Python. I also have experience working with popular web development frameworks such as React, Angular, Vue and Django.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://ik.imagekit.io/s3tynwsqe/portfolio/Screenshot_from_2023-03-20_12-43-11_b8ND_dua-A.png?updatedAt=1679305402398",
+        // desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        tech: ["React.js", "Tailwind CSS", "WordPress"],
+        link: "https://www.vascometh.com/",
+    },
+    {
+        img: "https://ik.imagekit.io/s3tynwsqe/portfolio/Screenshot_from_2023-03-20_12-40-40_GHAe_o_Ef.png?updatedAt=1679305254460",
+        // desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        tech: ["HTML", "CSS", "Javascript"],
+        link: "https://gracious-ride-37384a.netlify.app/",
+    },
+    {
+        img: "https://ik.imagekit.io/s3tynwsqe/portfolio/Screenshot_from_2023-03-20_12-45-36_yWfj8AZL6X.png?updatedAt=1679305556014",
+        // desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        tech: ["NEXT.js", "Tailwind CSS"],
+        link: "https://thefutur-clone.vercel.app/",
+    },
+    {
+        img: "https://ik.imagekit.io/s3tynwsqe/portfolio/Screenshot_from_2023-03-20_12-47-28_VzSOWpAYDA.png?updatedAt=1679305660530",
+        // desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://nft-drop-gray.vercel.app/",
+        tech: ["NEXT.js", "Tailwind CSS", "Sanity CMS","Thirdweb"],
+    },
+    {
+        img: "https://ik.imagekit.io/s3tynwsqe/portfolio/Screenshot_from_2023-03-20_12-49-30__o-H1Iy5fT.png?updatedAt=1679305784968",
+        // desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        tech: ["NEXT.js", "Tailwind CSS", "Map Box"],
+        link: "https://airbnb-clone-yoni1342.vercel.app/",
+    },
+    {
+        img: "https://ik.imagekit.io/s3tynwsqe/portfolio/Screenshot_from_2023-03-20_14-44-53_n8YDie2MO.png?updatedAt=1679312723307",
+        // desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        tech: ["Angular.js", "Tailwind CSS"],
+        link: "https://e-commerce-with-angular.vercel.app/",
+    },
+
+    {
+        img: "https://ik.imagekit.io/s3tynwsqe/portfolio/Screenshot_from_2023-03-20_14-47-56_WX7kADeg7L.png?updatedAt=1679312890232",
+        // desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        tech: ["NEXT.js", "Tailwind CSS"],
+        link: "https://portfolio-yoni1342.vercel.app/",
+    },
+];
+
+const contactConfig = {
+    Intro :"Hello there! Thank you for taking the time to visit my website. If you have any questions or inquiries, please don't hesitate to contact me. Here are a few ways to get in touch:",
+    YOUR_EMAIL: "yonatantesfaye30@gmail.com",
+    YOUR_FONE: "+2519 9353 4293",
+    description: "Feel free to reach out to me through any of these channels, and I'll get back to you as soon as possible. Whether you're looking for a web developer to work on a project, have a question about a blog post, or just want to say hello, I'm always happy to hear from fellow tech enthusiasts.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_anq38zk",
+    YOUR_TEMPLATE_ID: "template_3fwfern",
+    YOUR_USER_ID: "wt-aCOTUQWl8z_26H",
+};
+
+const socialprofils = {
+    github: "https://github.com/yoni1342/",
+    // facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/yonatan-tesfaye-8594211a8/",
+    telegram: "https://t.me/Yonatan_Tesfaye"
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
